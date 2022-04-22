d302e9fcf5f76bbe75ee1529ba81f5c4478e07c2
Prema <kouba.prem@gmail.com>
2022-04-22 07:34:06 +0200

    added missing default layout and doc

---
+++ DayOfFun/DayOfFun/Models/Question.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DayOfFun.Model
+{
+    public class Question
+    {
+        [ForeignKey("QuestionId")]
+        public int Id { get; set; }
+        
+        [Display(Name = "Question text")]
+        [Required(ErrorMessage = "Question text is required")]
+        [StringLength(1000, MinimumLength = 1, ErrorMessage ="text must be between 1 and 1000 chars")]
+        public String Text { get; set; }
+
+        public Boolean Enabled { get; set; }
+
+        public String? Tags { get; set; }
+
+        public virtual ICollection<Quiz> Quizzes { get; set; }
+
+        public Question()
+        {
+            this.Text = "DEFAULT";
+        }
+    }
+}
---
+++ DayOfFun/DayOfFun/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using DayOfFun.Models;
+
+namespace DayOfFun.Model
+{
+    public class User
+    {
+        [ForeignKey("UserId")]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public String Email { get; set; }
+
+        public User()
+        {
+            this.Name = "UNKNOWN";
+            this.Email = "UNKNOWN";
+        }
+        
+        //RELATIONSHIPTS
+        public virtual List<Quizzes_Users> Quizzes_Users { get; set; }
+    }
+}
